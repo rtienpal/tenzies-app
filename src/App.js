4db8d53a77a94a91feb3cd013f9265bb75faacfb
Rafael Uen <rafaeluen@gmail.com>
2022-06-06 12:17:07 -0300

    added dices and roll button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 import React from "react";
+import Die from "./components/Die";
 
 export default function App() {
-  return <main>123</main>;
+  const [diceNumbers, setDiceNumbers] = React.useState(allNewDice());
+
+  function allNewDice() {
+    const randomDieNumbersArray = [];
+    for (let i = 0; i < 10; i++) {
+      randomDieNumbersArray[i] = Math.floor(Math.random() * 7);
+    }
+    return randomDieNumbersArray;
+  }
+
+  function rollDice() {
+    setDiceNumbers(allNewDice);
+  }
+
+  const die = diceNumbers.map((number) => <Die number={number}></Die>);
+  return (
+    <main>
+      <div className="dice-container">{die}</div>
+      <button className="roll-button" onClick={rollDice}>
+        Roll
+      </button>
+    </main>
+  );
 }
---
+++ src/components/Die.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Die(props) {
+  return (
+    <div className="die-face">
+      <h2 className="die-num">{props.number}</h2>
+    </div>
+  );
+}
